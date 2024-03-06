a71b8358b2ae3aaa061956d9692e6b0dcf314bf0
Solomon Yeboah <modelmaestrosolo@gmail.com>
2024-03-06 18:27:37 +0000

    minor touches on home page

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,9 +1,54 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
 
 ## page configuration settings
 st.set_page_config(
     page_title="customer-churn", page_icon=":repeat: :wave:", layout="wide"
 )
+
+
+def display_contact():
+    st.sidebar.write("# Contact Information")
+    st.sidebar.write(
+        """
+    Let's connect. Your feedbacks are welcomed!
+    """
+    )
+
+    # columns = st.sidebar.columns(6)
+
+    # with columns[1]:
+    #     st.write(
+    #         """<div style="width:100%;text-align:center;"><a href="https://streamlit.io" style="float:center"><img src="http://www.doigtdecole.com/wp-content/uploads/2020/03/logo-rond-twitter.png" width="22px"></img></a></div>""",
+    #         unsafe_allow_html=True,
+    #     )
+
+    # with columns[2]:
+    #     st.write(
+    #         """<div style="width:100%;text-align:center;"><a href="https://streamlit.io" style="float:center"><img src="http://www.doigtdecole.com/wp-content/uploads/2020/03/logo-rond-twitter.png" width="22px"></img></a></div>""",
+    #         unsafe_allow_html=True,
+    #     )
+
+    # with columns[3]:
+    #     st.write(
+    #         """<div style="width:100%;text-align:center;"><a href="https://streamlit.io" style="float:center"><img src="http://www.doigtdecole.com/wp-content/uploads/2020/03/logo-rond-twitter.png" width="22px"></img></a></div>""",
+    #         unsafe_allow_html=True,
+    #     )
+
+    st.sidebar.write(
+        """<div style="width:100%;text-align:center;">
+        <a href="https://github.com/modelMaestroSolo" style="float:center">
+        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="35px"></img></a>
+        <a href="https://twitter.com/modelmaestroSol" style="float:center">
+        <img src="http://www.doigtdecole.com/wp-content/uploads/2020/03/logo-rond-twitter.png" width="35px"></img></a>
+        <a href="https://www.linkedin.com/in/yebsolomon" style="float:center">
+        <img src="https://i.pinimg.com/736x/96/8e/a6/968ea62882943e88bbd318ae5fa67429.jpg" width="35px"></img></a>
+        <a href="mailto:modelmaestrosolo@gmail.com" style="float:center">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png" width="35px"></img></a>
+        </div>""",
+        unsafe_allow_html=True,
+    )
 
 
 ## put title element in container
@@ -34,15 +79,19 @@ with st.container(border=True):
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("Key Features")
+    st.subheader("Key Features")
     st.markdown(
-        """
-                - 
-                
+        """     
+                - **A Data Page:** explore the content of proprietory data loaded in real-time form the remote server
+                - **A Dashboard Page:** presents visualizations on both the exploratory data and the KPIs
+                - **A Predict Page:** predict customer churn using a selected model of choice
+                - **A History Page:** contains saved predictions for further analysis later. Users can view the history of their prediction input values
         """
     )
 
-    st.header("User Benefits")
+
+with col2:
+    st.subheader("User Benefits")
     st.markdown(
         """
                 - Make data-driven decisions by leveraging the power of predictive analytics
@@ -50,9 +99,12 @@ with col1:
                 - Simple and straight forward. 
                 """
     )
+    st.subheader("Instruction")
+    st.markdown(
+        """
+                - Use the side bar to navigate the pages of the App
+                - Follow instructions on each page to interact with the App
+                """
+    )
 
-with col2:
-    st.header("Instructions ")
-    st.header("Contact Info")
-    st.header("Feedback")
-    st.write("Your feedbacks are welcomed. contact me at modelmaestrosolo@gmail.com")
+display_contact()
